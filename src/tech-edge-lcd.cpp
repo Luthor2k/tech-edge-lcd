e6609f8e41fda4e472e0065f8bd5f24d00eb028c
Arthur <arthur.hazleden@gmail.com>
2023-07-22 22:47:22 -0700

    serial working after ttl to rs232 fix

--- src/tech-edge-lcd.cpp
+++ src/tech-edge-lcd.cpp
@@ -20,30 +20,29 @@ static const uint8_t SETTING_STATUS_LED = 13; //chip select pin
 const uint8_t SETTING_ENGINE_PULSES_PER_REV = 1;
 const float SETTING_STOIC_AFR = 14.5; //AFR of 14.5 is correct for a diesel, 14.7 for gas
 
-//light dependent resistor for display brightness
-int LDRreading;     //raw LDR value; 700 when dark, under 20 when bright
+const uint8_t SETTING_LCD_CONTRAST = 50;
+int LDRreading;     //light dependent resistor for display brightness, raw values are about 700 when dark, under 20 when bright
 int LDRState = 1;  // will be set to one of four brightness levels, start out at 1
 
-SerLCD lcd; // Initialize the library with default I2C address 0x72
 
-RTC_DS3231 rtc;
-
-HardwareSerial DAQSerialPort(1);  //if using UART1
 
 //program function timing
 unsigned long prevDisplayUpdateTime = 0;
-const uint64_t SETTING_DISPLAY_UPDATE_INTERVAL_MS = 100;
+const uint64_t SETTING_DISPLAY_UPDATE_INTERVAL_MS = 1000;
 
 unsigned long prevBrightnessUpdateTime = 0;
 const uint64_t SETTING_BRIGHTNESS_UPDATE_INTERVAL_MS = 5000; //set this to how often to update the brightness in milliseconds
 
+unsigned long DAQserialFrameStartTime = 0;
+const uint64_t SETTING_DAQ_SERIAL_FRAME_WAIT_MS = 50; //approx time for the serial frame to definetly complete
+
 //Serial stream from controller
 const uint8_t DATA_FRAME_frameheader1 = 0x5a;
 const uint8_t DATA_FRAME_frameheader2 = 0xa5;
-const uint8_t DATA_FRAME_RX_LENGTH_BYTES = 28;
-const uint8_t SETTING_SERIAL_FRAME_RX_DELAY_MS = 15; //approx time for the serial frame to definetly complete
-uint8_t incomingSerial[DATA_FRAME_RX_LENGTH_BYTES];
-uint8_t serialStreamCount = 0;
+const uint8_t DATA_FRAME_RX_LENGTH_BYTES = 28;   //there are 28 bytes in a correctly formatted frame
+byte incomingSerial[20];
+uint8_t DAQbytesAvailable = 0;
+bool DAQsetToRead = false;
 
 // Raw data coming in from controller
 uint8_t frameheader1 = 0x5A;
@@ -109,8 +108,15 @@ uint8_t crcComp;
 
 bool crcGood;
 
+SerLCD lcd; // Initialize the library with default I2C address 0x72
+
+RTC_DS3231 rtc;
+
+HardwareSerial PCSerialPort(0);
+HardwareSerial DAQSerialPort(1);  //UART1, connected to the rs232 converter and then to the DAQ
+
 //function declarations
-bool check_serial();
+void check_serial();
 void update_averages();
 void update_display();
 void update_brightness();
@@ -118,10 +124,10 @@ void remap_raw_values();
 
 void setup() {
   //initialize ports
-  Serial.begin(115200);
+  PCSerialPort.begin(115200);
   Wire.begin();
+  pinMode(16, INPUT);
   DAQSerialPort.begin(19200, SERIAL_8N1, 16, 17);
-  delay(20);
   pinMode(SETTING_STATUS_LED, OUTPUT);
 
   //lcd setup
@@ -129,50 +135,51 @@ void setup() {
   delay(20);
   Wire.setClock(400000); //Optional - set I2C SCL to High Speed Mode of 400kHz
   //lcd.setBacklight(255, 255, 255); //bright white
-  lcd.setContrast(60); //Set contrast. Lower to 0 for higher contrast.
+  lcd.setContrast(SETTING_LCD_CONTRAST); //Set contrast. Lower to 0 for higher contrast.
   lcd.clear();
-  delay(20);
+  /*
   lcd.setCursor(0, 0);
   lcd.print("tech-edge-esp32-test");
   lcd.setCursor(0, 2);
   lcd.print("compiled ");
   lcd.print(String(__DATE__));
-  /*
+  */
+  
   //SD card setup
   if(!SD.begin(SETTING_SD_CARD_CS)){
-      Serial.println("Card Mount Failed");
+      PCSerialPort.println("Card Mount Failed");
       return;
   }
   uint8_t cardType = SD.cardType();
 
   if(cardType == CARD_NONE){
-      Serial.println("No SD card attached");
+      PCSerialPort.println("No SD card attached");
       return;
   }
 
-  Serial.print("SD Card Type: ");
+  PCSerialPort.print("SD Card Type: ");
   if(cardType == CARD_MMC){
-      Serial.println("MMC");
+      PCSerialPort.println("MMC");
   } else if(cardType == CARD_SD){
-      Serial.println("SDSC");
+      PCSerialPort.println("SDSC");
   } else if(cardType == CARD_SDHC){
-      Serial.println("SDHC");
+      PCSerialPort.println("SDHC");
   } else {
-      Serial.println("UNKNOWN");
+      PCSerialPort.println("UNKNOWN");
   }
 
   uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+  PCSerialPort.printf("SD Card Size: %lluMB\n", cardSize);
 
   //rtc setup
   if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
+    PCSerialPort.println("Couldn't find RTC");
+    PCSerialPort.flush();
     while (1) delay(10);
   }
 
   if (rtc.lostPower()) {
-    Serial.println("RTC lost power, let's set the time!");
+    PCSerialPort.println("RTC lost power, let's set the time!");
     // When time needs to be set on a new device, or after a power loss, the
     // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
@@ -188,93 +195,153 @@ void setup() {
   // January 21, 2014 at 3am you would call:
   // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
 
-  */
+  
 
 
   /*
   DateTime now = rtc.now();
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
+  PCSerialPort.print(now.year(), DEC);
+  PCSerialPort.print('/');
+  PCSerialPort.print(now.month(), DEC);
+  PCSerialPort.print('/');
+  PCSerialPort.print(now.day(), DEC);
+  PCSerialPort.print(" ");
+  PCSerialPort.print(now.hour(), DEC);
+  PCSerialPort.print(':');
+  PCSerialPort.print(now.minute(), DEC);
+  PCSerialPort.print(':');
+  PCSerialPort.print(now.second(), DEC);
+  PCSerialPort.println();
   */
 }
 
 void loop() {
   unsigned long currentMillis = millis();
 
-  if (check_serial() == true){
-    update_averages();
-    remap_raw_values();
+  //DAQ sending 28chars (1 frame) at 10Hz, 19200 baud
+  //(1/19200) * 28 = 1.458333ms per frame **seems to take longer than 2ms.. wait 20 instead
+  //1 frame sent every 100ms; 100 - 1.45833 = 0.0985416s = 98ms seconds between frames
+  if (DAQSerialPort.available() && DAQsetToRead == false) {
+    DAQserialFrameStartTime = currentMillis;
+    DAQbytesAvailable = DAQSerialPort.available();
+    PCSerialPort.println(DAQbytesAvailable);
+    DAQsetToRead = true;
   }
+
+  if (currentMillis - DAQserialFrameStartTime >= SETTING_DAQ_SERIAL_FRAME_WAIT_MS && DAQsetToRead == true) {
+    DAQbytesAvailable = DAQSerialPort.available();
+    PCSerialPort.println(DAQbytesAvailable);
+    DAQSerialPort.readBytes(incomingSerial, DAQbytesAvailable);
+    int totalBytes = DAQbytesAvailable;
+    while (DAQbytesAvailable > 0) {
+      PCSerialPort.print(incomingSerial[totalBytes - DAQbytesAvailable], HEX);
+      incomingSerial[totalBytes - DAQbytesAvailable] = 0;
+      DAQbytesAvailable--;
+    }
+    PCSerialPort.print("\n");
+    DAQsetToRead = false;
+  }
+  
+
+  if (crcGood == true){
+    PCSerialPort.println("serial frame sync");
+    //update_averages();
+    //remap_raw_values();
+    
+  }
+
 
   if (currentMillis - prevDisplayUpdateTime >= SETTING_DISPLAY_UPDATE_INTERVAL_MS) {
     prevDisplayUpdateTime = currentMillis;
-    update_display();
+    //update_display();
   }
   
   if (currentMillis - prevBrightnessUpdateTime >= SETTING_BRIGHTNESS_UPDATE_INTERVAL_MS) {
     prevBrightnessUpdateTime = currentMillis;
-    update_brightness();
+    //update_brightness();
   }
   
 }
 
-bool check_serial()
-{
-  if (DAQSerialPort.available() > 0) {
-    
-    digitalWrite(SETTING_STATUS_LED, HIGH);
+void check_serial() {
+  crcGood = false;
+  digitalWrite(SETTING_STATUS_LED, HIGH);
+  delay(100);
+  /*
+  //delay(SETTING_SERIAL_FRAME_RX_DELAY_MS);  //pause for the time of 1 frame to complete
+  DAQSerialPort.readBytesUntil(DATA_FRAME_frameheader1, incomingSerial, DATA_FRAME_RX_LENGTH_BYTES);
+  Serial.println(F(incomingSerial));
+  //this ^ would mean the frame is syncd from the back end not the front, can this be used as the index instead?
+  DAQSerialPort.readBytesUntil(DATA_FRAME_frameheader1, incomingSerial, DATA_FRAME_RX_LENGTH_BYTES);
+  Serial.println(F(incomingSerial));
+  */
+  //read until the end of the likely partial frame
+  int bytesRead = DAQSerialPort.readBytes(incomingSerial, DATA_FRAME_RX_LENGTH_BYTES);
+  //then read until the end of the known good frame
+  //DAQSerialPort.readBytesUntil(DATA_FRAME_frameheader1, incomingSerial, DATA_FRAME_RX_LENGTH_BYTES);
+  PCSerialPort.print("bytesRead: "+bytesRead);
 
-    delay(SETTING_SERIAL_FRAME_RX_DELAY_MS);  //pause for the time of 1 frame to complete
+  for (byte headerSearchIndex = 0; headerSearchIndex < DATA_FRAME_RX_LENGTH_BYTES; headerSearchIndex++) {
+    PCSerialPort.print(incomingSerial[headerSearchIndex], HEX);    
+  }
+  PCSerialPort.println(" END");
 
-    DAQSerialPort.readBytesUntil(DATA_FRAME_frameheader1, incomingSerial, DATA_FRAME_RX_LENGTH_BYTES);
-    digitalWrite(SETTING_STATUS_LED, LOW);
+  //find the start of frame header, a two byte code;   1   - Frame Header byte 1 (0x5A), 2   - Frame Header byte 2 (0xA5)
+  bool headerSearchFirstByteFound = false;
+  for (byte headerSearchIndex = 0; headerSearchIndex < DATA_FRAME_RX_LENGTH_BYTES; headerSearchIndex++) {
 
-    if (incomingSerial[0] == byte(DATA_FRAME_frameheader2))    //should be the second of the 0x5A 0xA5 frame header
-    {
-      // CRC calculation
-      crcSum = 0;
-      for (uint8_t frameByteIndex = 0; frameByteIndex < 27; frameByteIndex++){
-        crcSum += incomingSerial[frameByteIndex];
-        crcComp = lowByte(crcSum);  //should ALWAYS be 0xFF. well, should always be 0xA5 because of how the frame sync is being done..
+    PCSerialPort.print(headerSearchIndex, DEC);
+    PCSerialPort.print(":");
+    PCSerialPort.println(incomingSerial[headerSearchIndex], HEX);
+
+    if (incomingSerial[headerSearchIndex] == DATA_FRAME_frameheader1) {
+
+      PCSerialPort.println("found first header frame");
+
+      if (incomingSerial[headerSearchIndex + 1] == DATA_FRAME_frameheader2) {
+
+        PCSerialPort.println("found second header frame");
+        headerSearchFirstByteFound = true;
+        
       }
 
-      crcGood = false;
-      if (crcComp == DATA_FRAME_frameheader2){
-        crcGood = true;
-  
-        /// Break apart the frame: ///
-        sequence_counter = incomingSerial[1];
-    
-        DAQRawLambda16 = (int(incomingSerial[4]) * 256) + int(incomingSerial[5]);
-    
-        DAQRawUser1 = (int(incomingSerial[8]) * 256) + int(incomingSerial[9]);      //boost pressure
-        DAQRawUser2 = (int(incomingSerial[10]) * 256) + int(incomingSerial[11]);    //throttle position
-        DAQRawUser3 = (int(incomingSerial[12]) * 256) + int(incomingSerial[13]);    //intake temperature
-    
-        DAQRawThermocouple1 = (int(incomingSerial[14]) * 256) + int(incomingSerial[15]);  //egt, needs correcting curve to be applied
-        DAQRawThermocouple2 = (int(incomingSerial[16]) * 256) + int(incomingSerial[17]);  //oil temp
-        DAQRawThermocouple3 = (int(incomingSerial[18]) * 256) + int(incomingSerial[19]);  //turbo temp
-    
-        DAQRawOnboardThermistor = (int(incomingSerial[20]) * 256) + int(incomingSerial[21]);
-        DAQRawRPMCount = (int(incomingSerial[22]) * 256) + int(incomingSerial[23]);
-    
-        DAQRawSensorState = byte(incomingSerial[24]) & byte(0x07);  //wideband pump cell pid state bits
-        DAQRawHeaterState = byte(incomingSerial[25]) & byte(0x07);  //wideband heater pid state bits
-        return true;
-      }
     }
   }
-  return false;
+
+  digitalWrite(SETTING_STATUS_LED, LOW);
+  /*
+  if (incomingSerial[0] == byte(DATA_FRAME_frameheader2))    //should be the second of the 0x5A 0xA5 frame header
+  {
+    // CRC calculation
+    crcSum = 0;
+    for (uint8_t frameByteIndex = 0; frameByteIndex < 27; frameByteIndex++){
+      crcSum += incomingSerial[frameByteIndex];
+      crcComp = lowByte(crcSum);  //should ALWAYS be 0xFF. well, should always be 0xA5 because of how the frame sync is being done..
+    }
+    if (crcComp == DATA_FRAME_frameheader2){
+      crcGood = true;
+
+      /// Break apart the frame: ///
+      sequence_counter = incomingSerial[1];
+  
+      DAQRawLambda16 = (int(incomingSerial[4]) * 256) + int(incomingSerial[5]);
+  
+      DAQRawUser1 = (int(incomingSerial[8]) * 256) + int(incomingSerial[9]);      //boost pressure
+      DAQRawUser2 = (int(incomingSerial[10]) * 256) + int(incomingSerial[11]);    //throttle position
+      DAQRawUser3 = (int(incomingSerial[12]) * 256) + int(incomingSerial[13]);    //intake temperature
+  
+      DAQRawThermocouple1 = (int(incomingSerial[14]) * 256) + int(incomingSerial[15]);  //egt, needs correcting curve to be applied
+      DAQRawThermocouple2 = (int(incomingSerial[16]) * 256) + int(incomingSerial[17]);  //oil temp
+      DAQRawThermocouple3 = (int(incomingSerial[18]) * 256) + int(incomingSerial[19]);  //turbo temp
+  
+      DAQRawOnboardThermistor = (int(incomingSerial[20]) * 256) + int(incomingSerial[21]);
+      DAQRawRPMCount = (int(incomingSerial[22]) * 256) + int(incomingSerial[23]);
+  
+      DAQRawSensorState = byte(incomingSerial[24]) & byte(0x07);  //wideband pump cell pid state bits
+      DAQRawHeaterState = byte(incomingSerial[25]) & byte(0x07);  //wideband heater pid state bits
+    }
+  }
+  */
 }
 
 void update_averages()
